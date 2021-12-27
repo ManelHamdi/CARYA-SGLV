6ebe6436a44a22604c092ea4aa198fb48c7a9264
hamdi.manelmh <hamdi.manelmh@gmail.com>
2021-12-27 17:12:22 +0100

    dashboard

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,6 +2,8 @@
 
 namespace App\Http\Controllers;
 
+use Illuminate\Support\Facades\DB;
+
 class HomeController extends Controller
 {
     /**
@@ -21,6 +23,19 @@ class HomeController extends Controller
      */
     public function index()
     {
-        return view('dashboard');
+        // nbr v dispo
+        // nbr v en location
+        // nbr v en panne
+        // nbr v en reparation
+        // v dispo / jour
+        // nbr v / client
+        // lst tbl en retour aujourd'hui
+        // visite technique proche
+        // DB::table('users')->count();
+        $vdispo = DB::table('vehicules')->where('disponibilite', 1)->count();
+        $vlocation = DB::table('contrats')->count();
+        $nbrv = DB::table('vehicules')->count();
+
+        return view('dashboard', ['vdispo' => $vdispo, 'vlocation' => $vlocation, 'nbrv' => $nbrv]);
     }
 }
