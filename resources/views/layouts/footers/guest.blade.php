be6c284c5a2d9d4ccea09b70c72af9e8d7376189
hamdi.manelmh <hamdi.manelmh@gmail.com>
2021-12-27 18:26:25 +0100

    footer

--- resources/views/layouts/footers/guest.blade.php
+++ resources/views/layouts/footers/guest.blade.php
@@ -2,7 +2,7 @@
     <div class="container">
 
         <div class="copyright float-right">
-        SGLV &copy;
+        CARYA &copy;
         <script>
             document.write(new Date().getFullYear())
         </script>, made with <i class="material-icons">favorite</i> by
